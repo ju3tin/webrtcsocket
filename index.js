5d043541c6b1ab800e05b9f352477a8e3e3d6c38
ju3tin <ju3tin@hotmail.co.uk>
2024-11-22 20:04:57 +0000

    sdf

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const io = new Server(server, {
     },
 });
 
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'Hello, this is your GET API response!' });
+});
+
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
