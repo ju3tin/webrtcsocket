35d812d8ac956c336d9e681ca12b8e9dc4a50827
Justin <ju3tin@hotmail.co.uk>
2024-11-22 21:34:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,52 @@ app.get('/api/data', (req, res) => {
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
+
+socket.on('join', (roomId) => {
+    const selectedRoom = io.sockets.adapter.rooms.get(roomId)
+    const numberOfClients = selectedRoom ? selectedRoom.size : 0
+
+    if (numberOfClients === 0) {
+      console.log(`Creating room ${roomId} and emitting room_created socket event`)
+      socket.join(roomId)
+      socket.emit('room_created', roomId)
+    } else if (numberOfClients === 1) {
+      console.log(`Joining room ${roomId} and emitting room_joined socket event`)
+      socket.join(roomId)
+      socket.emit('room_joined', roomId)
+    } else {
+      console.log(`Can't join room ${roomId}, emitting full_room socket event`)
+      socket.emit('full_room', roomId)
+    }
+  })
+
+  // These events are emitted to all the sockets connected to the same room except the sender.
+  socket.on('start_call', (roomId) => {
+    console.log(`Broadcasting start_call event to peers in room ${roomId}`)
+    socket.to(roomId).emit('start_call')
+  })
+
+  socket.on('webrtc_offer', (event) => {
+    console.log(`Broadcasting webrtc_offer event to peers in room ${event.roomId}`)
+    socket.to(event.roomId).emit('webrtc_offer', event.sdp)
+  })
+
+  socket.on('webrtc_answer', (event) => {
+    console.log(`Broadcasting webrtc_answer event to peers in room ${event.roomId}`)
+    socket.to(event.roomId).emit('webrtc_answer', event.sdp)
+  })
+
+  socket.on('webrtc_ice_candidate', (event) => {
+    console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`)
+    socket.to(event.roomId).emit('webrtc_ice_candidate', event)
+  })
+  socket.on('send_message', (data) => {
+    socket.to(data.roomId).emit('receive_message', {
+        sender: 'User', // You can replace this with the actual user identifier
+        message: data.message,
+    })
+
+    
     // Relay signaling messages
     socket.on('signal', (data) => {
         const { to, signal } = data;
